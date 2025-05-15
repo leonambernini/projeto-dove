4a71aa718bc2dfb96b56478ee8691f480ae7fed3
Leonam Bernini <leonam.b@hotmail.com>
2025-05-15 13:50:25 -0300

    t

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const apiRoutes = require('./routes/api');
 const proxyRoutes = require('./routes/proxy');
 const path = require('path');
@@ -16,11 +15,7 @@ app.set('views', path.join(__dirname, 'templates')); // pasta dos templates
 app.set('view engine', 'ejs');
 
 app.use('/api', apiRoutes);
-// app.use('/proxy', proxyRoutes);
-
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('MongoDB conectado!'))
-//     .catch((err) => console.error('Erro ao conectar no MongoDB:', err));
+app.use('/proxy', proxyRoutes);
 
 const PORT = process.env.PORT || 3444;
 app.listen(PORT, () => {
